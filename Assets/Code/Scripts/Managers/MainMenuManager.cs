32f48dec0a1cfb42b9ae63ab0d17020ffaefb2be
dkrivchun@gmail.com <>
2024-04-29 20:34:47 -0700

    Adding some settings to menu

---
+++ Assets/Code/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
+using System.Xml.Serialization;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject AccountFlow;
+    public GameObject settingsMenu;
+    public GameObject mainMenu;
+    public TMP_Dropdown graphicsDropdown;
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer mainAudioMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Button button = GetComponent<Button>();
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application (works in standalone builds)
+        Application.Quit();
+        Debug.Log("Quitting game");
+    }
+
+    public void ToggleAccountFlow()
+    {
+        if (!AccountFlow.activeInHierarchy)
+        {
+            AccountFlow.SetActive(true);
+        }
+        else
+        {
+            AccountFlow.SetActive(false);
+        }
+    }
+    //Settings functions
+    public void OpenSettings() 
+    {
+        if (!settingsMenu.activeInHierarchy)
+        {
+            mainMenu.SetActive(false);
+            settingsMenu.SetActive(true);
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            settingsMenu.SetActive(false);
+        }
+    }
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        mainAudioMixer.SetFloat("masterVol", masterVol.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        mainAudioMixer.SetFloat("musicVol", musicVol.value);
+    }
+
+    public void ChangeSfxVolume()
+    {
+        mainAudioMixer.SetFloat("sfxVol", sfxVol.value);
+    }
+}
