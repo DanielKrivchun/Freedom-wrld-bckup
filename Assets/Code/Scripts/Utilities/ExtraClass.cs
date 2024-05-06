59c1f9590ad647c85850ba01ce79b8cc77c3feb9
kaushikjadavexpertappdev@gmail.com <>
2024-05-06 02:23:29 -0700

    #removed unwanted 3D models #added animated models #added animation controller script

--- Assets/Code/Scripts/Utilities/ExtraClass.cs
+++ Assets/Code/Scripts/Utilities/ExtraClass.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ExtraClass : MonoBehaviour
+
+public enum _AnimState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Idle,
+    Wallk,
+    Run,
+    Eating,
+    Jump,
+    Running
 }
