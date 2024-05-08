c5d936d01417614d32ef34124fdbb3ff53f8f23f
kaushikjadavexpertappdev@gmail.com <>
2024-05-08 10:24:43 -0700

    #added ai player #edited points for path genration

--- Assets/Code/Scripts/Managers/PathPointManager.cs
+++ Assets/Code/Scripts/Managers/PathPointManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PathPointManager : MonoBehaviour
 {
     public List<_WayPoints> m_way_points;
+    public bool m_randomized;
 
     public List<Vector3> _GetMyPath()
     {
@@ -12,11 +13,14 @@ public class PathPointManager : MonoBehaviour
         List<Vector3> v = new List<Vector3>();
 
         int m_count = m_way_points.Count;
-        int m_random_no = Random.Range(0,5);
+        int m_random_no = Random.Range(0, 5);
 
         for (int i = 0; i < m_count; i++)
         {
-            //m_random_no = Random.Range(0, m_way_points[i].m_points.Length);
+            if (m_randomized)
+            {
+                m_random_no = Random.Range(0, m_way_points[i].m_points.Length);
+            }
             v.Add(m_way_points[i].m_points[m_random_no].position);
         }
 #if UNITY_EDITOR
@@ -28,10 +32,15 @@ public class PathPointManager : MonoBehaviour
 
 #if UNITY_EDITOR
     public List<GameObject> m_lines;
+
+
     public void _GenratePathLines()
     {
 
         List<Vector3> v = _GetMyPath();
+
+        float m_d = 0;
+        
 
     }
 
