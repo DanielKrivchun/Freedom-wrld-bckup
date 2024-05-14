699174e7185abd1479bdc2ea7beacf11fa1dc753
kaushikjadavexpertappdev@gmail.com <>
2024-05-14 00:43:11 -0700

    #arrorow movement changed

--- Assets/Code/Scripts/UI/UiManager.cs
+++ Assets/Code/Scripts/UI/UiManager.cs
@@ -22,36 +22,65 @@ public class UiManager : MonoBehaviour
 
 
 
-
+    private Vector2 m_initial_pos;
     //MAX POSITION WILL GO HERE
     //LOW POS WILL BE ALWAYS ZERO AS ACNCOR SET
     private float m_max_y_pos = 600f;
 
-    private bool m_clicking;
+    public bool m_clicking;
 
     private Vector2 m_pos;
+    public float m_speed;
+    public float m_speedd;
+    [Space]
+    public float m_reset_t;
 
+
+    private float m_reset;
     private void Start()
     {
         m_pos = m_arrow.anchoredPosition;
         m_last_time = Time.time;
         m_currunt_time = Time.time;
+
+        m_initial_pos = m_arrow.anchoredPosition;
+
     }
 
     private void Update()
     {
+        m_reset += Time.deltaTime;
+        if (m_reset > m_reset_t)
+        {
+            m_clicking = false;
+        }
+
         if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
         {
+            m_reset = 0f;
             m_clicking = true;
             m_currunt_time = Time.time;
             m_diff = m_currunt_time - m_last_time;
             m_last_time = m_currunt_time;
+        }
+
+        if (m_clicking)
+        {
             _MoveArrow();
         }
+        else
+        {
+            _MoveArrowToInitialPos();
+        }
+
+    }
+
+    void _MoveArrowToInitialPos()
+    {
+        m_arrow.anchoredPosition = Vector2.Lerp(m_arrow.anchoredPosition, m_initial_pos, m_speed * Time.deltaTime);
     }
     void _MoveArrow()
     {
-
         if (m_diff > 1.5f)
         {
             m_diff = 1.5f;
@@ -61,7 +90,8 @@ public class UiManager : MonoBehaviour
         m_pos.y = -(m_max_y_pos) * (m_diff) / (1.5f);
 
         Debug.Log(m_diff + "               " + m_pos.y);
-        m_arrow.DOAnchorPos(m_pos, m_diff);
+        m_arrow.anchoredPosition = Vector2.Lerp(m_arrow.anchoredPosition, m_pos, m_speedd * Time.deltaTime);
+        //m_arrow.DOAnchorPos(m_pos, m_diff);
     }
 
 }
