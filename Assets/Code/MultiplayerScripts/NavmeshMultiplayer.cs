aec962196a4a1bc911f395413505e0c5b54d3946
kaushikjadavexpertappdev@gmail.com <>
2024-05-16 10:01:42 -0700

    #updated and solved navmesh navigation

--- Assets/Code/MultiplayerScripts/NavmeshMultiplayer.cs
+++ Assets/Code/MultiplayerScripts/NavmeshMultiplayer.cs
@@ -1,52 +1,36 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using Fusion;
+using Unity.VisualScripting;
 using UnityEngine.AI;
-
-public class NavMeshController : MonoBehaviour
+public class NavmeshMultiplayer : NetworkBehaviour
 {
-    #region PUBLIC
     [Header("SCRIPTABLE OBJECTS")]
     public InputValue m_input_value;
-    [Space]
-    public PathPointManager m_path_point;
-    #endregion
 
-    #region PRIVATE
-    private NavMeshAgent m_agent;
-    RaycastHit m_hit_info = new RaycastHit();
+    public List<Vector3> m_positions;
 
-    private Vector2 m_smooth_delta_pos = Vector2.zero;
-    private Vector2 m_velocity = Vector2.zero;
-
-    private List<Vector3> m_positions;
-    private float m_distance;
-
-    #endregion
-
-
-    private Camera m_camera;
     private int m_currunt_index;
+
+    [Space]
+    public PathPointManager PathPoint;
+
     private Vector3 m_currunt_pos;
 
-    #region UNITY METHODS
+    private float m_distance;
 
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent m_agent;
+
+    private void Start()
     {
-        m_camera = Camera.main;
-        m_agent = GetComponent<NavMeshAgent>();
+        PathPoint = FindObjectOfType<PathPointManager>();
+        _InitilizePath();
+        _SetDestination(m_positions[m_currunt_index]);
 
-        if (m_input_value.m_enable_navmesh)
-        {
-            _InitilizePath();
-            _SetDestination(m_positions[m_currunt_index]);
-        }
     }
 
-
-    private void Update()
+    public override void FixedUpdateNetwork()
     {
         if (!m_input_value.m_enable_navmesh)
         {
@@ -58,13 +42,9 @@ public class NavMeshController : MonoBehaviour
 
         if (m_distance < 1)
         {
-
             _ChangeCurruntPoint();
         }
-        //Debug.Log(m_distance);
-
     }
-    #endregion
 
     private void _ChangeCurruntPoint()
     {
@@ -77,14 +57,13 @@ public class NavMeshController : MonoBehaviour
         m_currunt_pos = m_positions[m_currunt_index];
         _SetDestination(m_positions[m_currunt_index]);
     }
-
     /// <summary>
     /// Initilize path
     /// </summary>
     void _InitilizePath()
     {
         m_positions = new List<Vector3>();
-        m_positions = m_path_point._GetMyPath();
+        m_positions = PathPoint._GetMyPath();
     }
 
     public void _SetDestination(Vector3 _target_pos)
@@ -96,5 +75,19 @@ public class NavMeshController : MonoBehaviour
     void _CalculateDistance()
     {
         m_distance = Vector3.Distance(m_currunt_pos, transform.position);
+        //Debug.Log(m_distance);
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 }
