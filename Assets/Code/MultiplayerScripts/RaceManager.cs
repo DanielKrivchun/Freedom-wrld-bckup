062733c00eb9f474a55af795372b32f8717c1f79
kaushikjadavexpertappdev@gmail.com <>
2024-07-09 06:16:55 -0700

    #updated code

--- Assets/Code/MultiplayerScripts/RaceManager.cs
+++ Assets/Code/MultiplayerScripts/RaceManager.cs
@@ -902,7 +902,7 @@ public class RaceManager : NetworkBehaviour, INetworkRunnerCallbacks
         _PlayerLeftDetails(_name);
     }
 
-    [Rpc(RpcSources.All, RpcTargets.All)]
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
     public void RPC_PlayerFinishedRace()
     {
         List<string> _ss = new List<string>();
